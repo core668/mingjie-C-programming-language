a21d2aa186abf5a75625d387c17f120e338e986f
core668 <403276838@qq.com>
2022-09-04 23:05:52 +0800

    chapter03代码

---
+++ chapter03/chapter03/list03.c
@@ -0,0 +1,187 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	/*
+	输入的整数能被5整除吗？
+	*/
+	/*
+	int no;
+	printf("请输入一个整数：");
+	scanf("%d", &no);
+
+	if (no % 5)
+		puts("输入的整数不能被5整除。");
+	*/
+
+	/*
+	输入的整数是奇数吗？
+	*/
+	/*
+	int no;
+	printf("请输入一个整数：");
+	scanf("%d", &no);
+
+	if (no % 2)
+		puts("输入的整数是奇数。");
+	*/
+
+	/*
+	输入的整数能被5整除吗？改进版
+	*/
+	/*
+	int no;
+	printf("请输入一个整数：");
+	scanf("%d", &no);
+
+	if (no % 5)
+		puts("输入的整数不能被5整除。");
+	else
+		puts("输入的整数能被5整除。");
+	*/
+
+	/*
+	输入的整数是奇数还是偶数？
+	*/
+	/*
+	int no;
+	printf("请输入一个整数：");
+	scanf("%d", &no);
+
+	if (no % 2)
+		puts("输入的整数是奇数。");
+	else
+		puts("该整数是偶数。");
+	*/
+
+	/*
+	输入两个整数，判断后者是否是前者的约数，a（倍数）能被b（约数）整除
+	*/
+	/*
+	int A, B;
+	puts("请输入两个整数。");
+
+	printf("整数A：");
+	scanf("%d", &A);
+
+	printf("整数B：");
+	scanf("%d", &B);
+
+	if (A % B)
+		puts("B不是A的约数");
+	else
+		puts("B是A的约数");
+	*/
+
+	/*
+	输入的整数值是否为0
+	*/
+	/*
+	int num;
+
+	printf("请输入一个整数：");
+	scanf("%d", &num);
+
+	if (num)
+		puts("该整数不为0");
+	else
+		puts("该整数为0");
+	*/
+
+	/*
+	输入的两个整数相等吗？
+	*/
+	/*
+	int n1, n2;
+
+	puts("请输入两个整数。");
+
+	printf("整数1：");
+	scanf("%d", &n1);
+
+	printf("整数2：");
+	scanf("%d", &n2);
+
+	if (n1 == n2)
+		puts("它们相等。");
+	else
+		puts("它们不相等");
+	*/
+
+	/*
+	输入的两个整数不相等吗？
+	*/
+	/*
+	int n1, n2;
+
+	puts("请输入两个整数。");
+
+	printf("整数1：");
+	scanf("%d", &n1);
+
+	printf("整数2：");
+	scanf("%d", &n2);
+
+	if (n1 != n2)
+		puts("它们不相等。");
+	else
+		puts("它们相等");
+	*/
+
+	/*
+	个位数是5吗？
+	*/
+	/*
+	int num;
+
+	printf("请输入一个整数：");
+	scanf("%d", &num);
+
+	if ((num % 10) == 5)
+		puts("该整数的个位数是5。");
+	else
+		puts("该整数的个位数不是5。");
+	*/
+
+	/*
+	判断输入的整数的符号
+	*/
+	/*
+	int no;
+	printf("请输入一个整数：");
+	scanf("%d", &no);
+
+	if (no == 0)
+		puts("该整数为0");
+	else if (no > 0)
+		puts("该整数为正数");
+	else
+		puts("该整数为负数");		
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
